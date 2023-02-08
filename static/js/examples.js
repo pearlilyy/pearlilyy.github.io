6ed25a13b7a2213db36889368928e244678e29c9
devpearl <flairpearllee@gmail.com>
2023-02-08 15:24:25 -0800

    project links combine

--- static/js/examples.js
+++ static/js/examples.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
-	$("#demosMenu").change(function(){
-	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
+$(document).ready(function () {
+	$("#demosMenu").change(function () {
+		// window.location.href = $(this).find("option:selected").attr("id") + '.html';
+		selected_pj = $(this).find("option:selected").attr("id")
+		if (selected_pj == 'gibson') {
+			window.open('https://pearls-nintendo.netlify.app', 'Pearls Nintendo Mobile', 'width=500, height=700');
+		} else if (selected_pj == 'starbucks') {
+			window.open('https://pearls-nintendo.netlify.app', 'Pearls Nintendo Mobile', 'width=500, height=700');
+		} else if (selected_pj == 'nintendo') {
+			window.open('https://pearls-nintendo.netlify.app', 'Pearls Nintendo Mobile', 'width=500, height=700');
+		} else if (selected_pj == 'coffeebean') {
+			window.open('https://pearls-coffeebean.netlify.app/', 'Pearls Coffee Bean', 'width=500, height=700');
+		} else if (selected_pj == 'subway') {
+			window.open('https://pearls-subway.netlify.app/');
+		} else {
+			window.open('https://pearls-subway.netlify.app/');
+		}
+
 	});
 });
