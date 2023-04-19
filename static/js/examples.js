38ef84c3d141f8c41cda8f3b8c0b8810b4d3c3fe
devpearl <flairpearllee@gmail.com>
2023-04-19 10:15:45 -0700

    project page update

--- static/js/examples.js
+++ static/js/examples.js
@@ -4,13 +4,13 @@ $(document).ready(function () {
 		if (selected_pj == 'gibson') {
 			window.open('https://github.com/pearlilyy/gibson', 'Pearls Gibson Guitar');
 		} else if (selected_pj == 'starbucks') {
-			window.open('https://pearls-nintendo.netlify.app', 'Pearls Starbucks', 'width=500, height=700');
+			window.open('https://pearls-starbucks.netlify.app', 'Pearls Starbucks', 'width=500, height=700');
 		} else if (selected_pj == 'nintendo') {
 			window.open('https://pearls-nintendo.netlify.app', 'Pearls Nintendo Mobile', 'width=500, height=700');
 		} else if (selected_pj == 'coffeebean') {
 			window.open('https://pearls-coffeebean.netlify.app/', 'Pearls Coffee Bean', 'width=500, height=700');
-		} else if (selected_pj == 'subway') {
-			window.open('https://pearls-subway.netlify.app/');
+		} else if (selected_pj == 'coco') {
+			window.open('https://pearls-coco.netlify.app/');
 		} else {
 			window.open('https://pearls-subway.netlify.app/');
 		}
