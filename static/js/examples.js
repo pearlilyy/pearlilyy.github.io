43229c7b665736aca9294bef93da9f5db1e9afce
devpearl <flairpearllee@gmail.com>
2023-11-05 18:28:01 -0800

    gibson website address edit

--- static/js/examples.js
+++ static/js/examples.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
 	$("#demosMenu").change(function () {
 		selected_pj = $(this).find("option:selected").attr("id")
 		if (selected_pj == 'gibson') {
-			window.open('https://github.com/pearlilyy/gibson', 'Pearls Gibson Guitar');
+			window.open('https://pearls-gibson.netlify.app', 'Pearls Gibson Guitar');
 		} else if (selected_pj == 'starbucks') {
 			window.open('https://pearls-starbucks.netlify.app', 'Pearls Starbucks', 'width=500, height=700');
 		} else if (selected_pj == 'nintendo') {
 			window.open('https://pearls-nintendo.netlify.app', 'Pearls Nintendo Mobile', 'width=500, height=700');
 		} else if (selected_pj == 'coffeebean') {
-			window.open('https://pearls-coffeebean.netlify.app/', 'Pearls Coffee Bean', 'width=500, height=700');
+			window.open('https://pearls-coffeebean.netlify.app', 'Pearls Coffee Bean', 'width=500, height=700');
 		} else if (selected_pj == 'coco') {
 			window.open('https://pearls-coco.netlify.app/');
 		} else {
